1cea5ab6c0b3dd0d7206844ff537358d2c45783a
Jose Arcani <juanjose.arcani2309@gmail.com>
2023-07-13 22:07:33 -0400

    refactor movie.py file to pass validations

--- routers/movie.py
+++ routers/movie.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Depends, Path, Query
 from fastapi.responses import JSONResponse
 from typing import List
 from config.database import Session
-from models.movie import Movie as MovieModel # rename to prevent conflict with class Movie
+from models.movie import Movie
+# from models.movie import Movie as MovieModel
+# rename to prevent conflict with class Movie
 from fastapi.encoders import jsonable_encoder
 from middlewares.jwt_bearer import JWTBearer
 from services.movie import MovieService
@@ -11,14 +13,22 @@ from schemas.movie import Movie
 movie_router = APIRouter()
 
 # @movie_router.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200)
-@movie_router.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
+@movie_router.get(
+        '/movies',
+        tags=['movies'],
+        response_model=List[Movie],
+        status_code=200,
+        dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
     db = Session()
     result = MovieService(db).get_movies()
     # result = db.query(MovieModel).all()
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
-@movie_router.get('/movies/{id}', tags=['movies'], response_model=Movie)
+@movie_router.get(
+        '/movies/{id}',
+        tags=['movies'],
+        response_model=Movie)
 def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     db = Session()
     result = MovieService(db).get_movie(id)
@@ -27,7 +37,10 @@ def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
         return JSONResponse(status_code=404, content={'message': "no encontrado"})
     return JSONResponse(content=jsonable_encoder(result))
 
-@movie_router.get('/movies/', tags=['movies'], response_model=List[Movie])
+@movie_router.get(
+        '/movies/',
+        tags=['movies'],
+        response_model=List[Movie])
 def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
     db = Session()
     result = MovieService(db).get_movies_by_category(category)
@@ -37,7 +50,11 @@ def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -
         return JSONResponse(status_code=404, content={'message': f"no encontrados {category}"})
     return JSONResponse(content=jsonable_encoder(result))
 
-@movie_router.post('/movies', tags=['movies'], response_model=dict, status_code=201)
+@movie_router.post(
+        '/movies',
+        tags=['movies'],
+        response_model=dict,
+        status_code=201)
 def create_movie(movie: Movie) -> dict:
     db = Session() # create session
     # MovieModel(title=movie.title, category=movie.category)
@@ -48,7 +65,11 @@ def create_movie(movie: Movie) -> dict:
     # movies.append(movie)
     return JSONResponse(status_code=201, content={"message": "Se ha registrado la película"})
 
-@movie_router.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+@movie_router.put(
+        '/movies/{id}',
+        tags=['movies'],
+        response_model=dict,
+        status_code=200)
 def update_movie(id: int, movie: Movie)-> dict:
     db = Session() # create session
     result = MovieService(db).get_movie(id)
@@ -57,7 +78,11 @@ def update_movie(id: int, movie: Movie)-> dict:
     MovieService(db).update_movie(id, movie)
     return JSONResponse(status_code=200, content={"message": "Se ha actualizado la película"})
 
-@movie_router.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+@movie_router.delete(
+        '/movies/{id}',
+        tags=['movies'],
+        response_model=dict,
+        status_code=200)
 def delete_movie(id: int)-> dict:
     db = Session() # create session
     result = MovieService(db).get_movie(id)
